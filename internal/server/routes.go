70d62e18cc4eafe94a488999bd8612df2475104f
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-17 22:43:37 -0300

    feat: add CORS middleware to server

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) registerRoutes() http.Handler {
@@ -16,6 +17,17 @@ func (s *Server) registerRoutes() http.Handler {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Use(cors.Handler(cors.Options{
+		// TODO: allow specific domains
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Handle("/web/*", http.StripPrefix("/web", handleServeWeb()))
 	r.Get("/healthz", handleHealthz(s.pubsub))
