0a68f22390a5c9e3989f211280ddc9d5ae0935c7
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-25 11:10:02 -0300

    ui: scroll message to view on render

--- web/src/components/messages/message.tsx
+++ web/src/components/messages/message.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 export type MessageProps = {
   /**
    * Sender username
@@ -8,8 +10,16 @@ export type MessageProps = {
 };
 
 export function Message({ message, username, date }: MessageProps) {
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (elementRef.current != null) {
+      elementRef.current.scrollIntoView();
+    }
+  }, []);
+
   return (
-    <div className="rounded-xl p-4 hover:bg-zinc-100">
+    <div ref={elementRef} className="rounded-xl p-4 hover:bg-zinc-100">
       <div className="flex items-center justify-between gap-4">
         <p className="cursor-pointer font-bold hover:underline">{username}</p>
         <p className="text-sm font-thin text-zinc-500" suppressHydrationWarning>
